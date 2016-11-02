62048c4a8ce12652526048978d9a36978dac9e69
sode <sode0719@gmail.com>
2016-11-02 15:05:15 +0900

    [update]eslint適応コンソールの色の調整

--- logger.js
+++ logger.js
@@ -1,70 +1,88 @@
-// =======================
-// コンソールカラー
-// =======================
-var black   = "\u001b[1;30m";
-var red     = "\u001b[1;31m";
-var green   = "\u001b[1;32m";
-var yellow  = "\u001b[1;33m";
-var blue    = "\u001b[1;34m";
-var magenta = "\u001b[1;35m";
-var cyan    = "\u001b[1;36m";
-var white   = "\u001b[1;37m";
+'use strict';
+//  =======================
+//  コンソールカラー
+//  =======================
+const black   = '\u001b[1;30m';
+const red     = '\u001b[31m';
+const green   = '\u001b[32m';
+const yellow  = '\u001b[33m';
+const blue    = '\u001b[34m';
+const magenta = '\u001b[35m';
+const cyan    = '\u001b[36m';
+const white   = '\u001b[37m';
 
-var reset   = "\u001b[0m";
+const reset   = '\u001b[0m';
 
-var MessageType = {
- INFO: cyan + "[INFO]" + reset,
- WARNING: yellow + "[WARNING]" + reset,
- ERROR: red + "[ERROR]" + reset,
- SUCCESS: green + "[SUCCESS]" + reset,
- FAILURE: red + "[FAILURE]" + reset,
- DEVELOP: black + "[DEVELOP]" + reset,
+const MessageType = {
+  INFO: cyan + '[INFO]' + reset,
+  WARNING: yellow + '[WARNING]' + reset,
+  ERROR: red + '[ERROR]' + reset,
+  SUCCESS: green + '[SUCCESS]' + reset,
+  FAILURE: red + '[FAILURE]' + reset,
+  DEVELOP: black + '[DEVELOP]' + reset,
 };
 
 function getTimestamp() {
-  var d = new Date();
-  var hour  = (d.getHours()   < 10) ? "0" + d.getHours()   : d.getHours();
-  var min   = (d.getMinutes() < 10) ? "0" + d.getMinutes() : d.getMinutes();
-  var sec   = (d.getSeconds() < 10) ? "0" + d.getSeconds() : d.getSeconds();
+  const d = new Date();
+  let hour = '';
+  if(d.getHours() < 10) {
+    hour = '0' + d.getHours();
+  } else {
+    hour = d.getHours();
+  }
 
-  var timestamp = hour + ":" + min + ":" + sec;
+  let min = '';
+  if(d.getMinutes() < 10) {
+    min = '0' + d.getMinutes();
+  } else {
+    min = d.getMinutes();
+  }
+
+  let sec = '';
+  if(d.getSeconds() < 10) {
+    sec = '0' + d.getSeconds();
+  } else {
+    sec = d.getSeconds();
+  }
+
+  const timestamp = hour + ':' + min + ':' + sec;
 
   return timestamp;
 }
 
 function setBracketsColor(timestamp, color) {
-  //色なし
-  return "[" + black + timestamp + reset +"]";
-  //括弧に色
-  //return color + "[" + reset + timestamp + color + "]" + reset;
-  //括弧と時間に色
-  //return color + "[" + timestamp + "]" + reset;
+  // 色なし
+  return '[' + black + timestamp + reset + ']';
+  // 括弧に色
+  // return color + '[' + reset + timestamp + color + ']' + reset;
+  // 括弧と時間に色
+  // return color + '[' + timestamp + ']' + reset;
 }
 
-exports.log = function (text) {
+exports.log = function(text) {
   console.log(setBracketsColor(getTimestamp(), white) + text);
-}
+};
 
-exports.info = function (text) {
+exports.info = function(text) {
   console.log(setBracketsColor(getTimestamp(), cyan) + MessageType.INFO + text);
-}
+};
 
-exports.warning = function (text) {
+exports.warning = function(text) {
   console.log(setBracketsColor(getTimestamp(), yellow) + MessageType.WARNING + text);
-}
+};
 
-exports.error = function (text) {
+exports.error = function(text) {
   console.log(setBracketsColor(getTimestamp(), red) + MessageType.ERROR + text);
-}
+};
 
-exports.success = function (text) {
-  console.log(setBracketsColor(getTimestamp(), green) + MessageType.SUCCESS + text + ":" + green + "Success" + reset);
-}
+exports.success = function(text) {
+  console.log(setBracketsColor(getTimestamp(), green) + MessageType.SUCCESS + text);
+};
 
-exports.failure = function (text) {
-  console.log(setBracketsColor(getTimestamp(), red) + MessageType.FAILURE + text + ":" + red + "Failure" + reset);
-}
+exports.failure = function(text) {
+  console.log(setBracketsColor(getTimestamp(), red) + MessageType.FAILURE + text);
+};
 
-exports.develop = function (text) {
+exports.develop = function(text) {
   console.log(setBracketsColor(getTimestamp(), black) + MessageType.DEVELOP + text);
-}
+};
